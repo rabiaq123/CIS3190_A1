85f1cd93f17ff8d8ccbf1e4dbbd676616b65716b
Rabia Qureshi <rqureshi@uoguelph.ca>
2022-02-01 21:57:49 -0500

    Changed all FFMC var names

--- ffwi.f95
+++ ffwi.f95
@@ -1,13 +1,14 @@
       program ffwi
-!     program no.: f-4
-!     fortran iv program to calculate canadian forest
-!     fire weather index for a dec pdp 11 at p.f.e.s.
+!     program no.: f-95
+!     fortran 95 program to calculate canadian forest
+!     fire weather index for a DEC PDP-11 computer.
 !     reads data and prints out in metric units.
       implicit none
 
-      real :: prev_ffmc, prev_dmc, prev_dc, noon_rain, rain, f, fr, ra, wmo, ed, ew, wm, z, x, ffm, noon_temp, temp 
+      real :: prev_ffmc, prev_dmc, prev_dc, noon_rain, rain, rain_func_ffmc, post_rain_ffmc, prev_rain 
+      real :: prev_mc, drying_emc, wetting_emc, curr_final_mc, intermediate_drying_rate, log_drying_rate, curr_ffmc, noon_temp, temp 
       real :: rk, pr, rw, wmi, b, wmr, dmc, pe
-      real :: smi, dr, fm, sf, si, bui, p, cc, bb, sl, fwi, c, dc
+      real :: smi, dr, fm, sf, si, bui, p, cc, bb, sl, fwi, correction_term_ffmc, dc
       integer :: j, l, i
       integer :: start_month, days_of_data, idays, days_in_month, noon_humidity, humidity, noon_wind, wind
       integer :: iffm, idmc, idc, isi
@@ -61,38 +62,38 @@
 !     fine fuel moisture code
       if(noon_rain>0.5) go to 10
       noon_rain=0.0
-      fr=prev_ffmc
+      post_rain_ffmc=prev_ffmc
       go to 150
-10    ra=noon_rain
-      if(ra<=1.45) go to 6
-      if(ra-5.75) 9,9,12
-6     f=123.85-(55.6*alog(ra+1.016))
+10    prev_rain=noon_rain
+      if(prev_rain<=1.45) go to 6
+      if(prev_rain-5.75) 9,9,12
+6     rain_func_ffmc=123.85-(55.6*alog(prev_rain+1.016))
       go to 13
-9     f=57.87-(18.2*alog(ra-1.016))
+9     rain_func_ffmc=57.87-(18.2*alog(prev_rain-1.016))
       go to 13
-12    f=40.69-(8.25*alog(ra-1.905))
-13    c=8.73*exp(-0.1117*prev_ffmc)
-      fr=(prev_ffmc/100.)*f+(1.0-c)
-      if(fr>=0.) go to 150
-      fr=0.0
-150   wmo=101.-fr
-      ed=0.942*(humidity**0.679)+(11.*exp((humidity-100.)/10.))+0.18*(21.1-noon_temp)*(1.-1./exp(0.115*humidity))
-      if(wmo-ed) 26,27,28
-26    ew=0.618*(humidity**0.753)+(10.*exp((humidity-100.)/10.))+0.18*(21.1-noon_temp)*(1.-1./exp(0.115*humidity))
-      if(wmo<ew) go to 29
-27    wm=wmo
+12    rain_func_ffmc=40.69-(8.25*alog(prev_rain-1.905))
+13    correction_term_ffmc=8.73*exp(-0.1117*prev_ffmc)
+      post_rain_ffmc=(prev_ffmc/100.)*rain_func_ffmc+(1.0-correction_term_ffmc)
+      if(post_rain_ffmc>=0.) go to 150
+      post_rain_ffmc=0.0
+150   prev_mc=101.-post_rain_ffmc
+      drying_emc=0.942*(humidity**0.679)+(11.*exp((humidity-100.)/10.))+0.18*(21.1-noon_temp)*(1.-1./exp(0.115*humidity))
+      if(prev_mc-drying_emc) 26,27,28
+26    wetting_emc=0.618*(humidity**0.753)+(10.*exp((humidity-100.)/10.))+0.18*(21.1-noon_temp)*(1.-1./exp(0.115*humidity))
+      if(prev_mc<wetting_emc) go to 29
+27    curr_final_mc=prev_mc
       go to 30
-28    z=0.424*(1.-(humidity/100.)**1.7)+(0.0694*(wind**0.5))*(1.-(humidity/100.)**8)
-      x=z*(0.463*(exp(0.0365*noon_temp)))
-      wm=ed+(wmo-ed)/10.**x
+28    intermediate_drying_rate=0.424*(1.-(humidity/100.)**1.7)+(0.0694*(wind**0.5))*(1.-(humidity/100.)**8)
+      log_drying_rate=intermediate_drying_rate*(0.463*(exp(0.0365*noon_temp)))
+      curr_final_mc=drying_emc+(prev_mc-drying_emc)/10.**log_drying_rate
       go to 30
-29    wm=ew-(ew-wmo)/1.9953
-30    ffm=101.-wm
-      if(ffm>101.) go to 32
-      if(ffm) 33,34,34
-32    ffm=101.
+29    curr_final_mc=wetting_emc-(wetting_emc-prev_mc)/1.9953
+30    curr_ffmc=101.-curr_final_mc
+      if(curr_ffmc>101.) go to 32
+      if(curr_ffmc) 33,34,34
+32    curr_ffmc=101.
       go to 34
-33    ffm=0.0
+33    curr_ffmc=0.0
 
 !     duff moisture code
 34    if(noon_temp+1.1>=0.) go to 41
@@ -101,8 +102,8 @@
 43    if(noon_rain>1.5) go to 45
       pr=prev_dmc
       go to 250
-45    ra=noon_rain
-      rw=0.92*ra-1.27
+45    prev_rain=noon_rain
+      rw=0.92*prev_rain-1.27
       wmi=20.0+280./exp(0.023*prev_dmc)
       if(prev_dmc<=33.) go to 50
       if(prev_dmc-65.) 52,52,53
@@ -122,8 +123,8 @@
       noon_temp=-2.8
 65    pe=(.36*(noon_temp+2.8)+day_length_dc(j))/2.
       if(noon_rain<=2.8) go to 300
-      ra=noon_rain
-      rw=0.83*ra-1.27
+      prev_rain=noon_rain
+      rw=0.83*prev_rain-1.27
       smi=800.*exp(-prev_dc/400.)
       dr=prev_dc-400.*alog(1.+((3.937*rw)/smi))
       if(dr>0.) go to 83
@@ -136,7 +137,7 @@
       dc=0.0
 
 !     initial spread index, buildup index, fire weather index
-85    fm=101.-ffm
+85    fm=101.-curr_ffmc
       sf=19.1152*exp(-0.1386*fm)*(1.+fm**4.65/7950000.)
       si=sf*exp(0.05039*wind)
 93    bui=(0.8*dc*dmc)/(dmc+0.4*dc)
@@ -155,14 +156,14 @@
       go to 400
 98    fwi=bb
 400   idc=dc+0.5
-      iffm=ffm+0.5
+      iffm=curr_ffmc+0.5
       idmc=dmc+0.5
       isi=si+0.5
       ibui=bui+0.5
       ifwi=fwi+0.5
       write(*,1001) j,i,temp,noon_humidity,noon_wind,rain,iffm,idmc,idc,isi,ibui,ifwi
 1001  format(1x,2i3,f6.1,i4,i6,f7.1,6i6)
-      prev_ffmc=ffm
+      prev_ffmc=curr_ffmc
       prev_dmc=dmc
       prev_dc=dc
 25    continue
