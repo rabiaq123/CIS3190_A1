0c2093ea57907758d80754dd49e7015b23e18aa5
Rabia Qureshi <rqureshi@uoguelph.ca>
2022-02-04 19:44:14 -0500

    Completed subroutine for reading file and storing data in main program

--- ffwi.f95
+++ ffwi.f95
@@ -8,7 +8,15 @@ implicit none
     ! variables for file reading
     character(len=20) :: fname
     logical :: lexist
-    
+    integer :: i
+    ! variables used for calculations
+    integer, dimension(12) :: len_month
+    real, dimension(12) :: day_length_dmc, day_length_dc
+    real :: prev_ffmc, prev_dmc, prev_dc
+    integer :: start_month, days_of_data, num_daily_entries
+    real, dimension(365) :: temp_arr, rain_arr
+    integer, dimension(365) :: humidity_arr, wind_arr
+
     write(*,1004)
     1004  format(5x,'FOREST FIRE WEATHER INDEX')
 
@@ -17,29 +25,39 @@ implicit none
     read (*,'(A)') fname
     inquire(file=fname, exist=lexist)
     if (lexist) then
-        write(*,*) 'reading the file'
-        call read_file(fname);
+        call read_file(fname, len_month, day_length_dmc, day_length_dc, prev_ffmc, prev_dmc, prev_dc, & 
+            start_month, days_of_data, num_daily_entries, temp_arr, rain_arr, humidity_arr, wind_arr)
     else
-        write (*,*) 'Invalid input file name.'
+        write (*,*) 'Invalid input file name. Exiting program...'
     end if 
-    
+
+    ! write data read from file for checking
+    do i=1,12
+        write(*,'(2x,i2,2x,f4.1,2x,f4.1)') len_month(i), day_length_dmc(i), day_length_dc(i)
+    end do 
+    write(*,'(2x,f4.1,2x,f4.1,2x,f5.1,2x,i2,2x,i2)') prev_ffmc, prev_dmc, prev_dc, start_month, days_of_data
+    do i=1,num_daily_entries
+        write(*,'(2x,f4.1,2x,i4,2x,i4,2x,f4.1)') temp_arr(i), humidity_arr(i), wind_arr(i), rain_arr(i)
+    end do
+
 end program ffwi
 
 
 ! read input file
-subroutine read_file(fname)
+subroutine read_file(fname, len_month, day_length_dmc, day_length_dc, prev_ffmc, prev_dmc, prev_dc, &
+    start_month, days_of_data, num_daily_entries, temp_arr, rain_arr, humidity_arr, wind_arr)
     implicit none 
     
     character (len=20), intent(in) :: fname
-    integer :: stat, index, num_daily_entries
+    integer :: stat, index
 
     ! variables used for calculations
-    integer, dimension(12) :: len_month
-    real, dimension(12) :: day_length_dmc, day_length_dc
-    real :: prev_ffmc, prev_dmc, prev_dc
-    integer :: start_month, days_of_data
-    real, dimension(365) :: temp_arr, rain_arr
-    integer, dimension(365) :: humidity_arr, wind_arr
+    integer, dimension(12), intent(out) :: len_month
+    real, dimension(12), intent(out) :: day_length_dmc, day_length_dc
+    real, intent(out) :: prev_ffmc, prev_dmc, prev_dc
+    integer, intent(out) :: start_month, days_of_data, num_daily_entries
+    real, dimension(365), intent(out) :: temp_arr, rain_arr
+    integer, dimension(365), intent(out) :: humidity_arr, wind_arr
 
     ! opening file to read; unit is used to represent file - can be anything but the value of 6
     open(unit=20,file=fname,status='old',action='read')
